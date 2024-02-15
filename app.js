237d6da374f9513480253a1eb8adc3a895b4ffdf
Caleb <calebcoliver@gmail.com>
2024-02-15 16:03:06 -0500

    Recipe Bar progress
    Implemented JSON parsing, and element building based on recipes.
    Fixed some formatting problems, and made buttons variable based. Need to work on color pallettes

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-//import * as data from jsonDataURL;
-const jsonDataURL = 'https://cables97.github.io/MealMaker/src/meals.json';
 
 //-------------------------------------
 //Fetch JSON Data from Github Page
 //-------------------------------------
+//import * as data from jsonDataURL;
+//const jsonDataURL = "https://cables97.github.io/MealMaker/src/meals.json";
+const jsonDataURL = './src/meals.json'
 async function fun() {
-    return fetch(jsonDataURL).then(res => res.json());
-    }
-    const data  = await fun();
+  return fetch(jsonDataURL).then(res => res.json());
+  }
+const data  = await fun();
 
 console.log(data);
-
-
-
-
 
 //----------------------------------
 // DOM Variables
 //----------------------------------
 const domRecipeList = document.getElementById('recipe-list')
-const domRecipeButton = document.getElementById('recipe-btn');
+const domRecipeButton = document.getElementById('recipe-open-btn');
+const domRecipeContainer = document.getElementById('recipe-container');
 
 
 const domShoppingList = document.getElementById('shopping-list');
-const domShoppingListButton = document.getElementById('s-l-btn');
+const domShoppingListButton = document.getElementById('s-l-open-btn');
 
 
+buildRecipes(data[0]);
 //----------------------------------
 // EventListeners Variables
 //----------------------------------
@@ -119,15 +118,20 @@ function toggleFilterItems(el){
         el.classList.add('f-i-include');
       }
 }
+console.log(data[0].ingredients[0][0].split(/-/).join(" "));
+console.log(data[0].ingredients[0][1][0] + " " + data[0].ingredients[0][1][1]);
+
 
 
 function buildRecipes(recipe){
 
   let recipeName = recipe.name;
-  let recipeCalories = recipe.calories
-  let recipeCookTime = recipe.cookTime;
+  let recipeCalories = recipe.calories + " cal";
+  let recipeCookDur = recipe.cookTime;
   let recipeIngredientList = recipe.ingredients;
   let recipeDirections = recipe.directions;
+
+ 
 
  //Recipe Box
   let recipeMaster = document.createElement("div");
@@ -148,16 +152,65 @@ function buildRecipes(recipe){
 
     //Recipe Contents
     let recipeItemContents = document.createElement('div');
-    recipeItemContents.classList.add('recipe-item-contents');
+    recipeItemContents.classList.add('recipe-item-content');
     recipeMaster.append(recipeItemContents);
 
+    let recipeInfo = document.createElement('div');
+    recipeInfo.classList.add('recipe-info');
+    recipeItemContents.append(recipeInfo);
+
+      let recipeTimeTrack = document.createElement('div');
+      recipeTimeTrack.classList.add('time-track');
+      recipeInfo.append(recipeTimeTrack);
+        
+        let recipeTimeClock = document.createElement('i');
+        recipeTimeClock.classList.add('fa-solid');
+        recipeTimeClock.classList.add('fa-clock');
+        recipeTimeClock.classList.add('fa-lg');
+        recipeTimeTrack.append(recipeTimeClock);
+
+        let recipeCookTime= document.createElement('p');
+        recipeCookTime.innerHTML = recipeCookDur;
+        recipeTimeTrack.append(recipeCookTime);
+
+      let recipeLink = document.createElement('a');
+      recipeLink.href = recipe.link;
+      recipeLink.innerHTML = 'Recipe';
+      recipeLink.target = "_blank"
+      recipeInfo.append(recipeLink);
+
+      let recipeLinkImg = document.createElement('i');
+      recipeLinkImg.classList.add('fa-solid');
+      recipeLinkImg.classList.add('fa-link');
+      recipeLink.append(recipeLinkImg);
+
+
+
     //FOR EACH RECIPE ITEM
+    recipeIngredientList.forEach(ingr => {
+      let qnty;
+      if(ingr[1][1] === "unit"){
+         qnty = ingr[1][0];
+      } else{
+         qnty = ingr[1][0] + " " + ingr[1][1];
+      }
 
-    let ingredientName = "";
+        let recipeIngredient = document.createElement('div');
+        recipeIngredient.classList.add('recipe-ingredient');
+        recipeItemContents.append(recipeIngredient);
 
-    let recipeIngredient = document.createElement('div');
-    recipeIngredient.classList.add('recipe-ingredient');
-    recipeItemContents.append(recipeIngredient);
+            let recipeIngredientName = document.createElement('p');
+            recipeIngredientName.innerHTML = ingr[0].split(/-/).join(" ");
+            recipeIngredient.append(recipeIngredientName);
+
+            let recipeIngredientAmnt = document.createElement('p');
+            recipeIngredientAmnt.innerHTML = qnty;
+            recipeIngredient.append(recipeIngredientAmnt);
+    });
+
+
+
+
 
 
       
